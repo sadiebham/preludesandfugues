546622960add76d95a2ffa353e2b89bb2e8aa87e
Sadie <sadiebham@gmail.com>
2026-05-15 19:35:48 +0100

    read through unrequited and piper finchling

--- makeall.sh
+++ makeall.sh
@@ -2,4 +2,9 @@ pdflatex preludesandfugues
 pdflatex preludesandfugues
 pdflatex preludesandfugues
 diff otherworld-1.tex otherworld.tex > otherworld.diff
+diff unrequitedsamsutton-1.tex unrequitedsamsutton.tex > unrequitedsamsutton.diff
+diff piperfinchling-1.tex piperfinchling.tex > piperfinchling.diff
+pdftk preludesandfugues.pdf cat 5-8 output introduction.pdf
+pdftk preludesandfugues.pdf cat 9-11 output unrequitedsamsutton.pdf
+pdftk preludesandfugues.pdf cat 17-26 output piperfinchling.pdf
 pdftk preludesandfugues.pdf cat 35-41 output otherworld.pdf
