3ffe13143b3dfe481d65e32f579ba58b89de19ef
Sadie <sadiebham@gmail.com>
2026-05-15 15:22:10 +0100

    Edits for otherworld done

---
+++ makeall.sh
@@ -0,0 +1,5 @@
+pdflatex preludesandfugues
+pdflatex preludesandfugues
+pdflatex preludesandfugues
+diff otherworld-1.tex otherworld.tex > otherworld.diff
+pdftk preludesandfugues.pdf cat 35-41 output otherworld.pdf
