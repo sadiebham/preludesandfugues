6f6383a0e73240c799d08c4c1c77af9edcb907e2
Sadie <sadiebham@gmail.com>
2026-05-19 14:07:36 +0100

    a few more minor changes

--- makeall.sh
+++ makeall.sh
@@ -4,7 +4,9 @@ pdflatex preludesandfugues
 diff otherworld-1.tex otherworld.tex > otherworld.diff
 diff unrequitedsamsutton-1.tex unrequitedsamsutton.tex > unrequitedsamsutton.diff
 diff piperfinchling-1.tex piperfinchling.tex > piperfinchling.diff
+diff ai-1.tex ai.tex > ai.diff
 pdftk preludesandfugues.pdf cat 5-8 output introduction.pdf
 pdftk preludesandfugues.pdf cat 9-11 output unrequitedsamsutton.pdf
 pdftk preludesandfugues.pdf cat 17-26 output piperfinchling.pdf
 pdftk preludesandfugues.pdf cat 35-41 output otherworld.pdf
+pdftk preludesandfugues.pdf cat 43-54 output ai.pdf
