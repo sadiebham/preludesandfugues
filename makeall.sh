adbf1454921fbfc66a1c3c5a4de9124d08f5fe0c
Sadie <sadiebham@gmail.com>
2026-05-27 15:44:58 +0100

    Lots of edits, two new pieces

--- makeall.sh
+++ makeall.sh
@@ -6,6 +6,7 @@ diff unrequitedsamsutton-1.tex unrequitedsamsutton.tex > unrequitedsamsutton.dif
 diff piperfinchling-1.tex piperfinchling.tex > piperfinchling.diff
 diff ai-1.tex ai.tex > ai.diff
 diff luke-1.tex luke.tex > luke.diff
+diff piano-1.tex piano.tex > piano.diff
 pdftk preludesandfugues.pdf cat 5-8 output introduction.pdf
 pdftk preludesandfugues.pdf cat 9-11 output unrequitedsamsutton.pdf
 pdftk preludesandfugues.pdf cat 17-26 output piperfinchling.pdf
