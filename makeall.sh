70cd81222bdd1aa374d1f91463c8145e9a60d087
Sadie <sadiebham@gmail.com>
2026-06-22 14:45:18 +0100

    minor charnges

--- makeall.sh
+++ makeall.sh
@@ -11,5 +11,5 @@ pdftk preludesandfugues.pdf cat 5-8 output introduction.pdf
 pdftk preludesandfugues.pdf cat 9-11 output unrequitedsamsutton.pdf
 pdftk preludesandfugues.pdf cat 17-26 output piperfinchling.pdf
 pdftk preludesandfugues.pdf cat 35-41 output otherworld.pdf
-pdftk preludesandfugues.pdf cat 43-54 output ai.pdf
+pdftk preludesandfugues.pdf cat 51-62 output ai.pdf
 pdftk preludesandfugues.pdf cat 27-30 output richard.pdf
